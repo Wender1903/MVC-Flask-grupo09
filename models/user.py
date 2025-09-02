c132bba9ac7c8785a2e02e4e4814339956453239
Wender1903 <wenderman74@gmail.com>
2025-09-01 22:52:44 -0300

    :sparkles: feat: Re-commitando config do bd q não tinha ido

--- models/user.py
+++ models/user.py
@@ -4,8 +4,10 @@ db = SQLAlchemy()
 
 # Cada classe que herda de db.Model se torna uma tabela no banco de dados.
 class User(db.Model): # Declaração de um novo modelo/tabela: User
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True) # Define uma coluna ... 
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
 
-    __tablename__ = 'users'
+    tasks = db.relationship('Task', back_populates='user')
